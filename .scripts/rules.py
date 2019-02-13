f709f8245fcd732672844852b3897bb07b4ab675
Nicholas Kirkby <nkirkby@e1z3r2p23.42.us.org>
2019-02-13 13:19:13 -0800

    checking for existence of tests in rules.py

--- .scripts/rules.py
+++ .scripts/rules.py
@@ -5,6 +5,7 @@ import shutil
 
 HERE = os.path.dirname(__file__)
 SRC_DIR = os.path.abspath(os.path.join(HERE, '..', 'src'))
+TESTS_DIR = os.path.abspath(os.path.join(HERE, '..', '.tests'))
 
 FUNCTION_NAMES = \
 ['memset',
@@ -36,10 +37,26 @@ FUNCTION_NAMES = \
 'toupper',
 'tolower']
 
-for function_name in FUNCTION_NAMES:
+def assert_definition_exists(function_name):
 	filename = os.path.join(SRC_DIR, f'ft_{function_name}.c')
 	try:
 		open(filename, 'x')
 	except FileExistsError:
 		pass
 	assert os.path.basename(filename) in os.listdir(SRC_DIR), f"No {filename} found"
+
+def assert_test_exists(function_name):
+	filename = os.path.join(TESTS_DIR, f'test_ft_{function_name}.c')
+	try:
+		open(filename, 'x')
+	except FileExistsError:
+		pass
+	assert os.path.basename(filename) in os.listdir(TESTS_DIR), f"No {filename} found"
+
+def assert_test_passes(function_name):
+	pass
+	
+if __name__ == '__main__':
+	for function_name in FUNCTION_NAMES:
+		assert_definition_exists(function_name)
+		assert_test_exists(function_name)

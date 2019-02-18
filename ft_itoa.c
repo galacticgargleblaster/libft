dee4baec562bc67ee6d4c409edfa3b31076456d2
Nicholas Kirkby <nkirkby@e1z4r12p4.42.us.org>
2019-02-18 00:20:11 -0800

    breaking ft_itoa -- needs test

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,43 +6,26 @@
 /*   By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/21 11:31:08 by nkirkby           #+#    #+#             */
-/*   Updated: 2019/02/16 11:33:16 by nkirkby          ###   ########.fr       */
+/*   Updated: 2019/02/18 00:17:36 by nkirkby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-/*
-**	Shamelessly copied from:
-**	https://github.com/illiatdesdindes/libft/blob/master/ft_itoa.c
-*/
-
-void	itoa_isnegative(long *n, int *negative)
-{
-	if (*n < 0)
-	{
-		*n *= -1;
-		*negative = 1;
-	}
-}
-
-char	*ft_itoa(long n)
+char	*ft_itoa(int n)
 {
 	long	tmpn;
 	long	len;
-	int		negative;
 	char	*str;
 
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-	tmpn = n;
+	tmpn = (long)n;
 	len = 2;
-	negative = 0;
-	itoa_isnegative(&n, &negative);
 	while (tmpn /= 10)
 		len++;
-	len += negative;
+	len += (n < 0);
 	if ((str = (char*)malloc(sizeof(char) * len)) == NULL)
 		return (NULL);
 	str[--len] = '\0';
@@ -51,7 +34,7 @@ char	*ft_itoa(long n)
 		str[len] = n % 10 + '0';
 		n = n / 10;
 	}
-	if (negative)
+	if (n < 0)
 		str[0] = '-';
 	return (str);
 }

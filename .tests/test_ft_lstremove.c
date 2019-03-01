9c25b27492442d3743dc3ee7bce086d4b80df1ce
Nicholas Kirkby <nkirkby@e1z4r15p3.42.us.org>
2019-02-28 18:47:34 -0800

    adding more test cases for lstremove

--- .tests/test_ft_lstremove.c
+++ .tests/test_ft_lstremove.c
@@ -9,14 +9,29 @@
 
 int main()
 {
-	t_list *lst = NULL; 
+	t_list *lst = NULL;
+	char *a = "AAA";
 	char *b = "BBB";
-	PUSH_STRING(lst, "AAA");
+	char *c = "CCC";
+	PUSH_STRING(lst, a);
 	PUSH_STRING(lst, b);
-	PUSH_STRING(lst, "CCC"); 
+	PUSH_STRING(lst, c); 
 
 	char *hmm = ft_lstremove(&lst, b);
 	assert(hmm == b);
 	assert(strcmp((char *)lst->content, "CCC") == 0);	
 	assert(strcmp((char *)lst->next->content, "AAA") == 0);
+
+	char *should_be_null = ft_lstremove(&lst, b);
+	assert(should_be_null == NULL);
+	assert(strcmp((char *)lst->content, "CCC") == 0);	
+	assert(strcmp((char *)lst->next->content, "AAA") == 0);
+
+	char *should_be_a = ft_lstremove(&lst, a);
+	assert(should_be_a == a);
+	assert(lst != NULL);
+
+	char *should_be_c = ft_lstremove(&lst, c);
+	assert(should_be_c == c);
+	assert(lst == NULL);
 }
